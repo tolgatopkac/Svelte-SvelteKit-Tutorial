fd1f7777b23a7bb6401c15d4a13de1a795ca006f
gmtolgatopkac <72669663+tolgatopkac@users.noreply.github.com>
2023-02-08 23:21:22 +0300

    each loops and objects with data file

--- Pokedex Tutorial/src/routes/+page.svelte
+++ Pokedex Tutorial/src/routes/+page.svelte
@@ -6,18 +6,27 @@
   // with object
   // ----------------------------------------------------------------
 
-  const greetingObj = {
-    greeting: "Hello",
-    name: "world",
-  };
+  // const greetingObj = {
+  //   greeting: "Hello",
+  //   name: "world",
+  // };
 
-  const data = ["yellow", "red", "blue"];
+  // const data = ["yellow", "red", "blue"];
+
+  import { generations } from "./generations";
 </script>
 
-<h1>{greetingObj.greeting} to {greetingObj.name}</h1>
+<!-- <h1>{greetingObj.greeting} to {greetingObj.name}</h1> -->
 
-{#each data as color, i (color)}
+<!-- {#each data as color, i (color)}
   <div>
     {i + 1}: {color},
   </div>
+{/each}
+ -->
+
+{#each generations as generation (generation.id)}
+  <h1>{generation.name}</h1>
+  <p>Games : {generation.games.join(", ")}</p>
+  <p>Main region: {generation.main_region}</p>
 {/each}
