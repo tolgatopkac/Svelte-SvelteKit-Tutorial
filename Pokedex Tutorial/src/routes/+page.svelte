408ae8c6c6eefe7d96f404c47aea0d833233fa21
gmtolgatopkac <72669663+tolgatopkac@users.noreply.github.com>
2023-02-09 09:01:48 +0300

    Reactivity Interaction types

--- Pokedex Tutorial/src/routes/+page.svelte
+++ Pokedex Tutorial/src/routes/+page.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import type { IndexMonster } from "./+page";
   import { generations } from "./generations";
   export let data: PageData;
+
+  let monsterId: string;
+  $: monster = data.monsters.find((monster) => monster.id === monsterId);
+  const monsterClick = (monster: IndexMonster) => {
+    monsterId = monster.id;
+  };
 </script>
+
+<h1>{monsterId}</h1>
+<h2>{monster?.name}</h2>
 
 <div class="generations">
   {#each generations as generation (generation.id)}
@@ -12,7 +22,8 @@
 
 <div class="monsters">
   {#each data.monsters as monster (monster.id)}
-    <div class="monster">
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="monster" on:click={() => monsterClick(monster)}>
       <div class="monster-content">
         <img src={monster.image} alt={monster.name} />
         {monster.name}
