a546ee2dc955e4c4849d171853747c729dffb4b1
gmtolgatopkac <72669663+tolgatopkac@users.noreply.github.com>
2023-02-10 09:48:02 +0300

    reactive statements

--- udemy/src/App.svelte
+++ udemy/src/App.svelte
@@ -1,18 +1,8 @@
-<!-- Script Tag -->
 <script>
-	let name = 'Svelte';
-	let src = 'http://picsum.com/200/200';
-	let string = 'Some <strong>string</strong> with html.';
+	import Counter from './lib/Counter.svelte';
 </script>
 
-<!-- HTML -->
-<img {src} alt="Some text {name}" />
-<h1>Hello {name.toLowerCase()}!</h1>
-<p>{@html string}</p>
+<Counter />
 
-<!-- style -->
 <style>
-	h1 {
-		color: red;
-	}
 </style>
