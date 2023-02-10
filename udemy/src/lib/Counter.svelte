f7edf9408a8089b96faad21da11b79bf33a1270d
gmtolgatopkac <72669663+tolgatopkac@users.noreply.github.com>
2023-02-10 23:21:08 +0300

    reactive statements gotchas

--- udemy/src/lib/Counter.svelte
+++ udemy/src/lib/Counter.svelte
@@ -1,19 +1,29 @@
 <script>
-	let count = 0;
-	// let string = `You have clicked ${count} times.`;
-	let string;
-	$: string = `You have clicked ${count} times.`;
+	let count1 = 0;
+	let count2 = 0;
 
-	$: if (count > 5) {
-		console.log(`The count is ${count}`);
+	function setCount2(x) {
+		count2 = x;
 	}
 
-	function increment() {
-		count += 1;
+	function getTotal(x, y) {
+		return x + y;
+	}
+
+	$: string = `The count total is ${getTotal(count1, count2)}`;
+	function increment1() {
+		count1 += 1;
+	}
+
+	$: setCount2(count1);
+	function increment2() {
+		count2 += 1;
 	}
 </script>
 
-<button on:click={increment}>Clicks {count}</button>
+<button on:click={increment1}>Clicks {count1}</button>
+<button on:click={increment2}>Clicks {count2}</button>
+
 <h3>{string}</h3>
 
 <style>
