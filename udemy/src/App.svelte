e78758a38a5ad8be661bd3fd9c1026674e537599
gmtolgatopkac <72669663+tolgatopkac@users.noreply.github.com>
2023-02-10 09:29:06 +0300

    basic svelte component

---
+++ udemy/src/App.svelte
@@ -0,0 +1,18 @@
+<!-- Script Tag -->
+<script>
+	let name = 'Svelte';
+	let src = 'http://picsum.com/200/200';
+	let string = 'Some <strong>string</strong> with html.';
+</script>
+
+<!-- HTML -->
+<img {src} alt="Some text {name}" />
+<h1>Hello {name.toLowerCase()}!</h1>
+<p>{@html string}</p>
+
+<!-- style -->
+<style>
+	h1 {
+		color: red;
+	}
+</style>
