b6e8763a3bcd9bb84a9a8b818dbfea1b5f3b801b
gmtolgatopkac <72669663+tolgatopkac@users.noreply.github.com>
2023-02-11 20:39:33 +0300

    basic component structure

---
+++ udemy_2/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Welcome from "./components/Welcome.svelte";
+  import Footer from "./components/Footer.svelte";
+</script>
+
+<Header />
+<Welcome />
+<Footer />
+
+<style>
+</style>
