229d32cacf06957028457bdb776c554657a883ce
gmtolgatopkac <72669663+tolgatopkac@users.noreply.github.com>
2023-02-13 11:00:14 +0300

    reactivity

---
+++ udemy_2/src/Card.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let urun;
+  export let fiyat;
+  export let aciklama;
+  export let satici;
+</script>
+
+<div>
+  <h3>Urun ismi: {urun}</h3>
+  <h4>Fiyat: {fiyat}</h4>
+  <p>Aciklama :{aciklama}</p>
+  <p>satici :{satici}</p>
+</div>
+
+<style>
+  div {
+    width: 300px;
+    background-color: aquamarine;
+  }
+</style>
