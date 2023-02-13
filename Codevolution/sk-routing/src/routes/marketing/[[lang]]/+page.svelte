d462d4f1f87d089dabd00045869f5d99c70f1ebe
gmtolgatopkac <72669663+tolgatopkac@users.noreply.github.com>
2023-02-13 10:58:54 +0300

    catch all routes/optional parametes

---
+++ Codevolution/sk-routing/src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+	const greetings = {
+		en: 'Hello!',
+		es: 'Holda!',
+		tr: 'Merhaba',
+		fr: 'Bonjour'
+	};
+
+	const { lang = 'en' } = $page.params;
+	const greeting = greetings[lang];
+</script>
+
+<h1>{greeting}</h1>
