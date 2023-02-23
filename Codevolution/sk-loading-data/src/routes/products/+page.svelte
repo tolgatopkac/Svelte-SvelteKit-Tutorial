d1240f5e23f2408127c50a2dc98d81f7d91082b1
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-02-23 21:37:34 +0300

    page data

--- Codevolution/sk-loading-data/src/routes/products/+page.svelte
+++ Codevolution/sk-loading-data/src/routes/products/+page.svelte
@@ -1,1 +1,13 @@
-<h1>List of Products</h1>
+<script>
+	export let data;
+	const products = data.products;
+</script>
+
+<h1>{data.title}</h1>
+{#each products as product}
+	<div>
+		<h2>{product.title}</h2>
+		<p>{product.description}</p>
+		<hr />
+	</div>
+{/each}
